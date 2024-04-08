8ffa9fde2a8bbc3266efe478d1f02501efad4fd3
Gitjamz25 <101423309+Gitjamz25@users.noreply.github.com>
2024-04-08 16:39:22 +0300

    Delete python-flask-api directory

--- python-flask-api/app/controllers/report.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template
-report_blueprint = Blueprint('report', __name__)
-@report_blueprint.route('', methods=['POST'])
-def index():
-    return render_template('layouts/report_layout.html')
